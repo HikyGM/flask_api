d147a3026378b53482877c0435438aa7c6b4e9fc
HikyGM <hikygm@yandex.ru>
2023-04-08 21:07:55 +0500

    refactor table Users, add GET, GET one user and register user POST

--- main.py
+++ main.py
@@ -265,6 +265,78 @@ def delete_posts(id_post):
         return jsonify({'error': 'Not found'})
 
 
+@application.route('/users/<int:id_user>', methods=['GET'])
+def get_one_user(id_user):
+    db_sess = db_session.create_session()
+    user = db_sess.query(model.User.User).get(id_user)
+    if not user:
+        return jsonify({'error': 'Not found'})
+    return jsonify(
+        {
+            'user': user.to_dict(only=(
+                'id_user', 'login_user', 'email_user', 'first_name_user', 'last_name_user',
+                'day_of_birth', 'gender_user', 'path_im_user', 'phone_number', 'city_user',
+                'type_user'))
+        }
+    )
+
+
+@application.route('/users', methods=['GET'])
+def get_users():
+    db_sess = db_session.create_session()
+    return jsonify(
+        {
+            'users':
+                [item.to_dict(only=('id_user', 'login_user', 'email_user',
+                                    'first_name_user', 'last_name_user'))
+                 for item in db_sess.query(model.User.User).all()]
+        }
+    )
+
+
+@application.route('/register', methods=['POST'])
+def register_user():
+    if not request.json:
+        return jsonify({'error': 'Empty request'})
+    elif not all(key in request.json for key in
+                 ['login_user', 'email_user', 'first_name_user', 'last_name_user',
+                  'day_of_birth', 'gender_user', 'phone_number', 'city_user',
+                  'password_user', 'repeat_password_user']):
+        return jsonify({'error': 'Bad request'})
+
+    no_value = {key: 'No value' for key, value in request.json.items() if not value}
+    if no_value:
+        return jsonify(no_value)
+    db_sess = db_session.create_session()
+    user = model.User.User
+    check_login = db_sess.query(user).filter(user.login_user == request.json['login_user']).first()
+    check_email = db_sess.query(user).filter(user.email_user == request.json['email_user']).first()
+    errors = {}
+    if check_login:
+        errors['login'] = 'Login is used'
+    if check_email:
+        errors['email'] = 'Email is used'
+    if request.json['password_user'] != request.json['repeat_password_user']:
+        errors['password'] = "Passwords don't match"
+    if errors:
+        return jsonify(errors)
+    add_user = user()
+    add_user.login_user = request.json['login_user']
+    add_user.password_user = user.set_password(user, request.json['password_user'])
+    add_user.email_user = request.json['email_user']
+    add_user.first_name_user = request.json['first_name_user']
+    add_user.last_name_user = request.json['last_name_user']
+    add_user.day_of_birth = request.json['day_of_birth']
+    add_user.gender_user = request.json['gender_user']
+    if 'path_im_user' in request.json:
+        add_user.path_im_user = request.json['path_im_user']
+    add_user.phone_number = request.json['phone_number']
+    add_user.city_user = request.json['city_user']
+    db_sess.merge(add_user)
+    db_sess.commit()
+    return jsonify({'success': 'OK'})
+
+
 def main():
     db_session.global_init()
 
--- static/database/User.py
+++ static/database/User.py
@@ -10,23 +10,24 @@ class User(SqlAlchemyBase, UserMixin, SerializerMixin):
     __tablename__ = 'users'
 
     id_user = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    login_user = sqlalchemy.Column(sqlalchemy.String(50), nullable=True)  # unique=True,
+    login_user = sqlalchemy.Column(sqlalchemy.String(50), nullable=True, unique=True)  # unique=True,
     password_user = sqlalchemy.Column(sqlalchemy.String(256), nullable=True)
     email_user = sqlalchemy.Column(sqlalchemy.String(50), index=True, unique=True, nullable=True)
     first_name_user = sqlalchemy.Column(sqlalchemy.String(50), nullable=True)
     last_name_user = sqlalchemy.Column(sqlalchemy.String(50), nullable=True)
     day_of_birth = sqlalchemy.Column(sqlalchemy.Date, nullable=True)
     gender_user = sqlalchemy.Column(sqlalchemy.String(50), nullable=True)
-    path_im_user = sqlalchemy.Column(sqlalchemy.String(50), nullable=True, default='up.png')
+    path_im_user = sqlalchemy.Column(sqlalchemy.String(50), nullable=True, default='static/images/nonePhoto.png')
     phone_number = sqlalchemy.Column(sqlalchemy.String(50), nullable=True)
     city_user = sqlalchemy.Column(sqlalchemy.String(50), nullable=True)
-    type_user = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('type_users.id_type'))
+    type_user = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('type_users.id_type'), default=3)
     type_u = orm.relationship('Type_users')
     blog = orm.relationship("Blog", back_populates='author')
     # id = id_user
 
     def set_password(self, password):
         self.password_user = generate_password_hash(password)
+        return generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_user, password)
